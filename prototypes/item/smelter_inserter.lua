1e4d79179bc7e6f8be5b5080a2ded85b6fb33b5a
pfg <omegariisthebest@gmail.com>
2016-09-22 17:52:21 -0700

    Fix __ModTest__ reference

--- prototypes/item/smelter_inserter.lua
+++ prototypes/item/smelter_inserter.lua
@@ -5,7 +5,7 @@ data:extend({
     order = "b-c-a",
     stack_size = 50,
     flags = { "goes-to-quickbar" },
-    icon = "__ModTest__/graphics/smelter_inserter_icon.png",
+    icon = "__liquid-science__/graphics/smelter_inserter_icon.png",
     place_result = "smelter-inserter"
   }
 })
