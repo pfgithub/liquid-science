819ef8066f5bc59be4c2b6edf6392a73ffd3139d
pfgithub <omegariisthebest@gmail.com>
2019-05-18 20:56:58 -0700

    Disable first science pack

--- prototypes/science/data.lua
+++ prototypes/science/data.lua
@@ -10,7 +10,7 @@ addScience = require("api.add_science")
 
 --208, 177, 21 high tech
 
-addScience("automation-science-pack", "0.8,0.2,0.1", "0.7,0.1,0.1", "default") -- order a
+-- addScience("automation-science-pack", "0.8,0.2,0.1", "0.7,0.1,0.1", "default") -- order a
 addScience("logistic-science-pack", "0.2,0.9,0.2", "0.1,0.8,0.2", "default") -- order b
 addScience("chemical-science-pack", "0.24,0.86,0.95", "0.22,0.68,0.77", "default") -- order c
 addScience("military-science-pack", "0.19607843137,0.2431372549,0.2862745098", "0.22,0.68,0.77", "default") -- order d
